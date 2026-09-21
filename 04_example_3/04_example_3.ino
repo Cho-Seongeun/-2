2e190422d5a9c5443cc239eb8c525a5a1adf0e77
Cho-Seongeun <prettyseoungeun@naver.com>
2026-09-21 23:26:35 +0900

    Refactor LED control logic in Arduino sketch

--- 04_example_3/04_example_3.ino
+++ 04_example_3/04_example_3.ino
@@ -5,19 +5,16 @@ void setup() {
 }
 
 void loop() {
-  // 1. 처음 1초 동안 LED 켜기 (Pin 7을 LOW로 설정해야 켜짐)
   digitalWrite(ledPin, LOW);
   delay(1000);
 
-  // 2. 다음 1초 동안 LED 5회 깜빡이기
   for (int i = 0; i < 5; i++) {
-    digitalWrite(ledPin, LOW);   // 켜짐
+    digitalWrite(ledPin, LOW); 
     delay(100);
-    digitalWrite(ledPin, HIGH);  // 꺼짐
+    digitalWrite(ledPin, HIGH); 
     delay(100);
   }
 
-  // 3. LED 끄기 및 무한 루프 (Pin 7을 HIGH로 설정해야 꺼짐)
   digitalWrite(ledPin, HIGH);
   while (1) {
   }
